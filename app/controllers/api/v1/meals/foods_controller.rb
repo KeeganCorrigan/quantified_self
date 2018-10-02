08daad7ab25c25a48b3185072263587d4e2c3f96
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-10-02 14:14:03 -0600

    adds test for meal foods post request, adds route and controller action, adds end point

--- app/controllers/api/v1/meals/foods_controller.rb
+++ app/controllers/api/v1/meals/foods_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::Meals::FoodsController < ApplicationController
   before_action :validate_meal, only: [:index]
+  before_action :set_meal_food, only: [:create]
 
   def index
     render json: Meal.includes(:foods).find_by(id: params[:meal_id])
+  end
+
+  def create
+    if @food && @meal
+      MealFood.create(meal: @meal, food: @food)
+      render json: { message: "Successfully added #{@food.name} to #{@meal.name}"}, status: 201
+    else
+      render status: 404
+    end
+  end
+
+  private
+
+  def set_meal_food
+    @food = Food.find_by(id: params[:id])
+    @meal = Meal.find_by(id: params[:meal_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :foods
       resources :meals, only: [:index] do
-        resources :foods, only: [:index], to: "meals/foods#index" do
-        end
+        post "/foods/:id", to: "meals/foods#create"
+        resources :foods, only: [:index], to: "meals/foods#index"
+        resources :foods, only: [:create], to: "meals/foods#create"
       end
     end
   end
